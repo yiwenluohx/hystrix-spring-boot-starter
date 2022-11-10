537a8761be56e16b5ec7f5dffb5fbcca56bcfb86
luohx <Lhongxiao@lcchain.com>
2022-11-10 18:30:28 +0800

    新增hystrix插件项目

---
+++ src/main/java/com/study/hystrix/HystrixSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.study.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/hystrix/HystrixSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
